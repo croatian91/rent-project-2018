21bb6471cf865f36535279efeffa1765f15e57c1
Ivan HANZEK <ivan.hanzek.hr@gmail.com>
2018-03-28 15:50:40 +0200

    import removed

--- src/main/java/fr/dauphine/rentproject2018/web/SessionController.java
+++ src/main/java/fr/dauphine/rentproject2018/web/SessionController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Date;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("cart")
